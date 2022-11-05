038a99915d0af404d2a5357489cda1e74f9d35d7
BrunoBGDev <bruno.braga.design@gmail.com>
2022-11-05 11:27:31 -0300

    Updated game_props.c

--- includes/game_props.h
+++ includes/game_props.h
@@ -6,15 +6,15 @@
 /*   By: bbraga <bruno.braga.design@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 22:27:53 by bbraga            #+#    #+#             */
-/*   Updated: 2022/11/05 10:26:47 by bbraga           ###   ########.fr       */
+/*   Updated: 2022/11/05 11:08:44 by bbraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GAME_PROPS_H
 # define GAME_PROPS_H
-# define FRAME_RATE			30
+# define FRAME_RATE			60
 # define BUF_SIZE			4098
-# define TILE_SIZE			16
+# define TILE_SIZE			64
 # define ENEMY				0
 # define SPAWN_RATE			3
 # define SCORE_LEN			4
